b65b3ff84433605077291ea54b659a97e68f6859
Arpit Gandhi <arpit.gandhi@ignitesol.com>
2016-09-17 10:25:34 +0530

    Initial Commit

---
+++ Movie-Search-Rohit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movie-Search-Rohit
+//
+//  Created by Arpit Gandhi on 17/09/16.
+//  Copyright © 2016 Ignite Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
